3f8cf1d302b1300949f032a7dae1f420232e47c5
Lili Syaripudin <lsyaripudin@gmail.com>
2025-11-07 16:49:57 +0700

    Tambah Lookup

--- Custom/Schemas/UsrBarangdanJasa1Page/UsrBarangdanJasa1Page.js
+++ Custom/Schemas/UsrBarangdanJasa1Page/UsrBarangdanJasa1Page.js
@@ -15,6 +15,7 @@ define("UsrBarangdanJasa1Page", [], function() {
 		}/**SCHEMA_DETAILS*/,
 		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
 		methods: {},
+		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
 		diff: /**SCHEMA_DIFF*/[
 			{
 				"operation": "insert",
@@ -35,48 +36,87 @@ define("UsrBarangdanJasa1Page", [], function() {
 			},
 			{
 				"operation": "insert",
-				"parentName": "Tabs",
-				"propertyName": "tabs",
-				"index": 0,
+				"name": "LOOKUPeb9b14df-777a-4b52-be52-88b20c8b2097",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrBarang",
+					"enabled": true,
+					"contentType": 5
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUP3c08787e-f7a9-4d09-bdc9-a35fa01b75f6",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrJasa",
+					"enabled": true,
+					"contentType": 5
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
 				"name": "NotesAndFilesTab",
 				"values": {
 					"caption": {
 						"bindTo": "Resources.Strings.NotesAndFilesTabCaption"
 					},
-					"items": []
-				}
+					"items": [],
+					"order": 0
+				},
+				"parentName": "Tabs",
+				"propertyName": "tabs",
+				"index": 0
 			},
 			{
 				"operation": "insert",
-				"parentName": "NotesAndFilesTab",
-				"propertyName": "items",
 				"name": "Files",
 				"values": {
-					"itemType": Terrasoft.ViewItemType.DETAIL
-				}
+					"itemType": 2
+				},
+				"parentName": "NotesAndFilesTab",
+				"propertyName": "items",
+				"index": 0
 			},
 			{
 				"operation": "insert",
-				"parentName": "NotesAndFilesTab",
-				"propertyName": "items",
 				"name": "NotesControlGroup",
 				"values": {
-					"itemType": Terrasoft.ViewItemType.CONTROL_GROUP,
+					"itemType": 15,
 					"caption": {
 						"bindTo": "Resources.Strings.NotesGroupCaption"
 					},
 					"items": []
-				}
+				},
+				"parentName": "NotesAndFilesTab",
+				"propertyName": "items",
+				"index": 1
 			},
 			{
 				"operation": "insert",
-				"parentName": "NotesControlGroup",
-				"propertyName": "items",
 				"name": "Notes",
 				"values": {
 					"bindTo": "UsrNotes",
-					"dataValueType": Terrasoft.DataValueType.TEXT,
-					"contentType": Terrasoft.ContentType.RICH_TEXT,
+					"dataValueType": 1,
+					"contentType": 4,
 					"layout": {
 						"column": 0,
 						"row": 0,
@@ -93,6 +133,16 @@ define("UsrBarangdanJasa1Page", [], function() {
 							"bindTo": "NotesImagesCollection"
 						}
 					}
+				},
+				"parentName": "NotesControlGroup",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "merge",
+				"name": "ESNTab",
+				"values": {
+					"order": 1
 				}
 			}
 		]/**SCHEMA_DIFF*/
