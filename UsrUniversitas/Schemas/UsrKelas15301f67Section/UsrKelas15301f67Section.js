1dec8848f743a495da919161277951ec431a5bbb
Lili Syaripudin <lsyaripudin@gmail.com>
2025-11-07 15:51:53 +0700

    Tambah Baru

---
+++ UsrUniversitas/Schemas/UsrKelas15301f67Section/UsrKelas15301f67Section.js
@@ -0,0 +1,8 @@
+define("UsrKelas15301f67Section", [], function() {
+	return {
+		entitySchemaName: "UsrKelas",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
