45e95f4c530ceba528a72eb78aeaf31e67c755d6
Lili Syaripudin <lsyaripudin@gmail.com>
2025-11-07 15:00:27 +0700

    Tambah Mahasiswa

---
+++ UsrMahasiswa/Schemas/UsrMahasiswa_Detail/UsrMahasiswa_Detail.js
@@ -0,0 +1,8 @@
+define('UsrMahasiswa_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrMahasiswa',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
